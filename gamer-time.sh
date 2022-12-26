735c227ddd0f61efebaf1318e909e134b3d66f92
TechTino <techtino74@gmail.com>
2022-12-26 16:00:21 +0000

    use gamemode with steam

--- gamer-time.sh
+++ gamer-time.sh
@@ -1,18 +1,23 @@
-# Gamer time 0 would revert it:
+# This is run over ssh so we need to tell the script what our DISPLAY is.
 export DISPLAY=:0
 case $1 in
+        # If ./gamer-time.sh 0 is called, turn off TV and turn on monitor etc.
 	0)
+	        # Lets shutdown steam, then wait a little bit because steam takes a bit to close properly.
 		steam -shutdown
 		sleep 1.2
+		
+		# Enable monitor and disable TV, change audio output
 		xrandr --output DP-4 --mode 3440x1440 --rate 143.92
 		xrandr --output HDMI-0 --off
 		pactl set-default-sink "alsa_output.pci-0000_0a_00.4.analog-stereo"
 		;;
 	1)
+	        # Turn off monitor and turn on TV, change audio output to hdmi, open big picture mode.
 		xrandr --output DP-4 --off
-		xrandr --output HDMI-0 --left-of DP-4 --auto
+		xrandr --output HDMI-0 --auto
 		pactl set-default-sink "alsa_output.pci-0000_08_00.1.hdmi-stereo"
-		steam -newbigpicture steam://open/bigpicture &
+		gamemoderun steam steam://open/bigpicture &
 		;;
 	*)
 		echo "Incorrect syntax";;
